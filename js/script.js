162495c9eac7ffcd63a086b6dad6ec7693f72bd4
Rabiul Jahid <79147422+RabiulJahid@users.noreply.github.com>
2021-06-06 20:36:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/*CONTENT CHANGE*/
+function textChange(){
+	document.getElementById("h3").innerHTML = "My first javescript Practice"
+}
+function TextChange(){
+	document.getElementById('h2').innerHTML = " lorem ipsam doller set omit";
+}
+
+
+function textchange(){
+	document.getElementById('out_Hover').innerHTML = "Thank You";
+
+}
+
+
+//CONTENT CHANGE
+
+//CSS CHANGE
+function cssChange(){
+	document.getElementById("cssChange").innerHTML = "JavaScript can change the style of an HTML element";
+}
+//CSS CHANGE
+//alert('i am new learner of js');
+//window.alert("i am rabiul jahid");
+//document.write("hello am happy")
+//console.log('i am rabiul jahid');
+//NUMBER CHANGE
+function numberChange(){
+   document.getElementById("rootp").innerHTML = ('5+6=11');
+}
+
+function colorChange(){
+	var x;
+	x = document.getElementById("commonp");
+	x.style.fontSize = "30px";
+	x.style.color = "red";
+	
+}
+function changeName(){
+var lastname, lastname;
+lastname = "Rabiul";
+lastname = "Jahid";
+document.getElementById("nameC").innerHTML = lastname;
+}
+
+function newText(){
+	var x;
+	x = document.getElementById("fiveh2").innerHTML = ('variable is one of the most important part js');
+		
+}
